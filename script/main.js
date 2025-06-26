1c1c0b754a77cea0110e2569405229ec88e712bf
Lucas Melo <123739531+lukemew@users.noreply.github.com>
2025-06-26 13:52:12 -0300

    Adicionando script do botão ver mais

--- script/main.js
+++ script/main.js
@@ -25,4 +25,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
   window.addEventListener("scroll", checkFadeIn);
   checkFadeIn();
+
+const verMaisBtn = document.getElementById("verMaisBtn");
+  const galleryContainer = document.querySelector(".gallery-images");
+
+  verMaisBtn.addEventListener("click", function (e) {
+    e.preventDefault(); 
+   
+    const novasFotos = [
+      "./assets/ambiente/barbearia-02.jpg",
+      "./assets/ambiente/barbearia-01.jpg",
+      "./assets/ambiente/barbearia-04.png",
+      "./assets/ambiente/barbearia-03.jpeg"
+    ];
+
+    novasFotos.forEach((foto, index) => {
+  const novaImg = document.createElement("img");
+  novaImg.src = foto;
+  novaImg.alt = "Foto Extra " + (index + 1);
+  novaImg.classList.add("scroll-fade", "show"); 
+
+  galleryContainer.appendChild(novaImg);
+});
+
+    checkFadeIn();
+    verMaisBtn.style.display = "none";
+  });
 });
