9cfc2921cff491f5353172b6b574ae5df9d1e36c
Lucas Melo <123739531+lukemew@users.noreply.github.com>
2025-06-26 14:07:01 -0300

    Adicionando evento para remover a classe no-js

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+document.documentElement.classList.remove("no-js");
+
 function toggleMenu() {
   const menu = document.getElementById("mobile-menu");
   menu.style.display = menu.style.display === "flex" ? "none" : "flex";
